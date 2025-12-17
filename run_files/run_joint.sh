4495725ffd1cf9cfc18ab44070241e97ca4296a3
Ryan Brady <ryan.m.brady@stonybrook.edu>
2025-12-17 16:01:49 -0500

    update joint bash logic

--- run_files/run_joint.sh
+++ run_files/run_joint.sh
@@ -5,19 +5,26 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=07:00:00
+#SBATCH --time=48:00:00
 
 name="J1620+1203"
 
-source activate env # activate conda env if necessary 
+source activate doubles # activate conda env if necessary 
 
 export OMP_NUM_THREADS=20
 export MKL_NUM_THREADS=20
 export NUMEXPR_NUM_THREADS=20
 
 # define the input and output notebook paths
-input_notebook="joint_model.ipynb"
+if [ ${name} == 'J1001+5027' ]; then
+    input_notebook="joint_model_j1001.ipynb"
+elif [ ${name} == 'J0602-4335' ]; then
+    input_notebook="joint_model_j0602.ipynb"
+else
+    input_notebook="joint_model.ipynb"
+fi
+
 output_notebook="joint_modeling/${name}/${name}_joint_model.ipynb"
 
 # execute the notebook via papermill
-papermill "$input_notebook" "$output_notebook" -f "yaml_files/F160W/${name}.yaml"
+papermill "$input_notebook" "$output_notebook" -f "yaml_files/joint_modeling/${name}.yaml"
